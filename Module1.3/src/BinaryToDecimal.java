5e13d8177f20bc7ab339bebc2921757b15e53a6a
AnnaLinden <linden.a.anna@gmail.com>
2023-10-24 22:09:29 +0300

    task2

--- Module1.3/src/BinaryToDecimal.java
+++ Module1.3/src/BinaryToDecimal.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// 11111 -> 31
 public class BinaryToDecimal {
 
     public static void main(String[] args) {
@@ -8,7 +9,23 @@ public class BinaryToDecimal {
         System.out.println("Enter a binary number:");
         String binary = scanner.nextLine();
 
-        int decimal = Integer.parseInt(binary, 2);
+        // Option 1: Use built-in method
+        // int decimal = Integer.parseInt(binary, 2);
+
+        // Option 2: Manually convert
+        int decimal = 0;
+        int length = binary.length();
+
+        for (int i = 0; i < length; i++) {
+            char bit = binary.charAt(i);
+            if (bit == '1') {
+                decimal += Math.pow(2, length - 1 - i);
+            } else if (bit != '0') {
+                System.out.println("Invalid binary input!");
+                return;
+            }
+        }
+
         System.out.println("Decimal equivalent: " + decimal);
     }
 }

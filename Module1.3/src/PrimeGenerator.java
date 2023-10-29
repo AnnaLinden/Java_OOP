1faab45e28e7c03ec4a82d330e8f72c284231d88
AnnaLinden <linden.a.anna@gmail.com>
2023-10-29 12:10:03 +0200

    task 3

--- Module1.3/src/PrimeGenerator.java
+++ Module1.3/src/PrimeGenerator.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
-// Write a program that prompts the user to enter two positive integers, start and end (where start < end). The program should generate and display all the prime numbers between start and end, inclusive.
-
-A prime number is a positive integer greater than 1 that has no positive divisors other than 1 and itself.
-
-Your program should use control structures(such as loops and conditional statements)to implement the logic for generating and checking prime numbers.
+// Write a program that prompts the user to enter two positive integers,
+// start and end (where start < end). The program should generate and display 
+// all the prime numbers between start and end, inclusive.
+// A prime number is a positive integer greater than 1
+// that has no positive divisors other than 1 and itself.
+// Your program should use control structures
+// (such as loops and conditional statements)to implement the logic
+// for generating and checking prime numbers.
 
 public class PrimeGenerator {
 

c6af879c640f2a0a82ad7e14d4a0a7f4c8ee6314
AnnaLinden <linden.a.anna@gmail.com>
2023-10-29 11:00:53 +0200

    Module 1.3 task1

--- Module1.3/src/QuadraticSolver.java
+++ Module1.3/src/QuadraticSolver.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 //2,-8,-24; roots:6;-2
+//Task 1: Quadratic equation solver
+
+// Write a program that prompts the user to enter the coefficients 
+// of a quadratic equation (ax^2 + bx + c = 0) and calculates its roots 
+// using the quadratic formula. Display the roots on the console. 
+// If the equation has no real roots, display the message "No real roots".
 
 public class QuadraticSolver {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter coefficient a:");
-        double a = scanner.nextDouble();
+        double a = getValidDouble(scanner, "Enter coefficient a:");
+        double b = getValidDouble(scanner, "Enter coefficient b:");
+        double c = getValidDouble(scanner, "Enter coefficient c:");
 
-        System.out.println("Enter coefficient b:");
-        double b = scanner.nextDouble();
-
-        System.out.println("Enter coefficient c:");
-        double c = scanner.nextDouble();
-
-        double discriminant = b * b - 4 * a * c;
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
 
         if (discriminant > 0) {
             double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
@@ -27,5 +28,16 @@ public class QuadraticSolver {
         } else {
             System.out.println("No real roots");
         }
+
     }
+
+    private static double getValidDouble(Scanner scanner, String prompt) {
+        System.out.println(prompt);
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Invalid input. Try again.");
+            scanner.next(); // clear invalid input
+        }
+        return scanner.nextDouble();
+    }
+
 }

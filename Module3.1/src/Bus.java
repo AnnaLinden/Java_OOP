9064681ad2ca906970615c447ee953aa726c3bd9
AnnaLinden <linden.a.anna@gmail.com>
2023-11-08 14:10:47 +0200

    bus

--- Module3.1/src/Bus.java
+++ Module3.1/src/Bus.java
@@ -3,19 +3,49 @@ public class Bus extends Car {
 
     public Bus(String typeName) {
         super(typeName);
-        passengers = 0;
+        this.passengers = 0;
     }
 
     public void passengerEnter(int count) {
-        passengers += count; // Add passengers
+        if (count < 0) {
+            System.out.println("Cannot enter a negative number of passengers.");
+        } else {
+            passengers += count; // Add passengers
+        }
     }
 
     public void passengerExit(int count) {
-        passengers = Math.max(0, passengers - count); // Remove passengers
+        if (count < 0) {
+            System.out.println("Cannot exit a negative number of passengers.");
+        } else if (count > passengers) {
+            System.out.println("Cannot exit more passengers than are on the bus.");
+        } else {
+            passengers -= count; // Remove passengers
+        }
     }
 
     public int getPassengers() {
         return passengers;
     }
 
+    public static void main(String[] args) {
+        // Test entering passengers
+        Bus bus = new Bus("Bus");
+        bus.passengerEnter(5);
+        System.out.println("Passengers after entering 5: " + (bus.getPassengers() == 5));
+
+        // Test exiting passengers
+        bus.passengerExit(3);
+        System.out.println("Passengers after exiting 3: " + (bus.getPassengers() == 2));
+
+        // Test exiting more passengers than are on the bus
+        bus.passengerExit(5);
+        System.out.println("Passengers after trying to exit 5: " + (bus.getPassengers() == 2));
+
+        // Test entering a negative number of passengers
+        bus.passengerEnter(-1);
+
+        // Test exiting a negative number of passengers
+        bus.passengerExit(-1);
+    }
 }

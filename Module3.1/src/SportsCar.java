bce47e227da072fec56e3cd14809639c9d0dd885
AnnaLinden <linden.a.anna@gmail.com>
2023-11-08 13:57:34 +0200

    SportsCar

--- Module3.1/src/SportsCar.java
+++ Module3.1/src/SportsCar.java
@@ -24,3 +24,63 @@ public class SportsCar extends Car {
     }
 
 }
+
+class SportsCarTest {
+
+    public static void shouldAccelerateFasterThanCar() {
+        Car normalCar = new Car("Car");
+        SportsCar sportsCar = new SportsCar("SportsCar");
+
+        normalCar.fillTank();
+        sportsCar.fillTank();
+
+        normalCar.accelerate();
+        sportsCar.accelerate();
+
+        System.out.println("Test Acceleration: " + (sportsCar.getSpeed() > normalCar.getSpeed()));
+    }
+
+    public static void shouldConsumeMoreGasolineOnAcceleration() {
+        SportsCar sportsCar = new SportsCar("SportsCar");
+        sportsCar.fillTank();
+        double beforeAcceleration = sportsCar.getGasolineLevel();
+
+        sportsCar.accelerate();
+
+        System.out.println("Test Gasoline Consumption: " +
+                (Math.abs(beforeAcceleration - 5 - sportsCar.getGasolineLevel()) < 0.01));
+    }
+
+    public static void shouldDecelerateFasterThanCar() {
+        Car normalCar = new Car("Car");
+        SportsCar sportsCar = new SportsCar("SportsCar");
+
+        normalCar.fillTank();
+        sportsCar.fillTank();
+
+        normalCar.setSpeed(100); //
+        sportsCar.setSpeed(100);
+
+        normalCar.decelerate(5); //
+        sportsCar.decelerate(5); //
+
+        System.out.println("Test Deceleration: " + (sportsCar.getSpeed() < normalCar.getSpeed()));
+    }
+
+    public static void shouldNotDecelerateBelowZero() {
+        SportsCar sportsCar = new SportsCar("SportsCar");
+        sportsCar.fillTank();
+        sportsCar.setSpeed(10); //
+
+        sportsCar.decelerate(10); //
+
+        System.out.println("Test Deceleration Below Zero: " + (sportsCar.getSpeed() == 0));
+    }
+
+    public static void main(String[] args) {
+        shouldAccelerateFasterThanCar();
+        shouldConsumeMoreGasolineOnAcceleration();
+        shouldDecelerateFasterThanCar();
+        shouldNotDecelerateBelowZero();
+    }
+}

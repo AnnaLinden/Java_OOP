d3fbf37f1bccafa37bf58b89efe6ceb11bf96d8a
AnnaLinden <linden.a.anna@gmail.com>
2023-12-09 11:25:51 +0200

    8.1 filtered people with lambda

--- Module_8.1_Lambda/src/main/java/Main.java
+++ Module_8.1_Lambda/src/main/java/Main.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
+    public static void main(String[] args) {
+        // Create a list of Person objects
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Mikko", 30, "Tammisaari"));
+        people.add(new Person("Liisa", 25, "Tampere"));
+        people.add(new Person("Johanna", 32, "Tammisaari"));
+        people.add(new Person("Antti", 23, "Turku"));
+        people.add(new Person("Kaisa", 27, "Oulu"));
+        people.add(new Person("Aki",50, "Helsinki"));
+        people.add(new Person("Orvokki", 70,"Vantaa"));
+
+        /// Sort the list by age using a lambda expression
+        // Comparator.comparingInt(Person::getAge) is a method reference that extracts the age for comparison
+        people.sort(Comparator.comparingInt(Person::getAge));
+
+        // Filter the list to include only people from "Tammisaari" using Predicate
+        // Define a Predicate for filtering
+        // A Predicate is a functional interface that represents a single argument function returning a boolean
+        Predicate<Person> isFromTammisaari = p -> "Tammisaari".equals(p.getCity());
+        List<Person> filteredPeople = new ArrayList<>();
+        for (Person person : people) {
+            // Apply the Predicate 'isFromTammisaari' to each person
+            // The 'test' method of Predicate returns true if the condition is met
+            if (isFromTammisaari.test(person)) {
+                filteredPeople.add(person);
+            }
+        }
+
+        // Print the filtered list
+        for (Person person : filteredPeople) {
+            System.out.println(person);
+        }
+    }
 }
--- Module_8.1_Lambda/src/main/java/Person.java
+++ Module_8.1_Lambda/src/main/java/Person.java
@@ -1,2 +1,35 @@
 public class Person {
+    private String name;
+    private int age;
+    private String city;
+
+    // Constructor
+    public Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    // toString method for easy printing
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
